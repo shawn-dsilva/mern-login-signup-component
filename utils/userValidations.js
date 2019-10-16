c79f9eef35a924e4eb593f2ba3e1f656ab7f09b6
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2019-10-16 15:19:18 +0530

    fixes

--- utils/userValidations.js
+++ utils/userValidations.js
@@ -6,9 +6,11 @@ const message = "Password must be between 6-16 characters, upper and lower case 
 const email = Joi.string().email({ minDomainSegments: 2}).required();
 
 const password = Joi.string().min(6).max(16).pattern(/^[a-zA-Z0-9]/).required().messages({
-  'string.pattern.base': `password should be a type of 'text'`,
+  'string.pattern.base': `Password can only contain upper case and lower case characters and numbers`,
   'string.empty': `Password cannot be an empty field`,
   'string.min': `Password should have a minimum length of {#limit}`,
+  'string.max': `Password should have a maximum length of {#limit}`,
+
 })
 // const password = Joi.string()
 // .pattern(/^[a-zA-Z0-9]{6,16}$/).error(new Error(message))
