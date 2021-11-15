c43cf6b9924f171df119cf42785a0d69e3c2bfa9
Shawn Dsilva <shawn@shawndsilva.com>
2021-11-15 16:41:09 +0530

    CSS Fixes

--- client/src/features/auth/Auth.js
+++ client/src/features/auth/Auth.js
@@ -6,23 +6,26 @@ function Auth({button}) {
 
     return (
         <div className='auth-container'>
-            <h1 > <strong>MERN</strong> Sessions Auth App </h1>
-            <p>Modern, Fully Featured Sessions based Authentication app <span role="img" aria-label="lock">🔒</span> and MERN starter template. </p>
-            <p>Featuring account confirmation through e-mail, password reset workflow, user avatar upload and more! </p>
-            <br></br>
-            <p>Built with Functional Component and Hooks based React, Redux Toolkit, NodeJS, Express and MongoDB</p>
-          <br/>
+            <div className='main-text'>
+                <h1 > <strong>MERN</strong> Sessions Auth App </h1>
+                <p>Modern, Fully Featured Sessions based Authentication app <span role="img" aria-label="lock">🔒</span> and MERN starter template. </p>
+                <p>Featuring account confirmation through e-mail, password reset workflow, user avatar upload and more! </p>
+                <br></br>
+                <p>Built with Functional Component and Hooks based React, Redux Toolkit, NodeJS, Express and MongoDB</p>
+            <br/>
 
-            <div style={{display:'flex', margin:'auto', justifyContent:'space-between'}}>
-                { button && <Link className='main-button' to="/login">
-                     Sign In
-                </Link>}
+                <div style={{margin:'2rem auto'}}>
+                    { button && <Link className='main-button' to="/login">
+                        Sign In
+                    </Link>}
 
-                {button && <Link className='main-button' to="/register">
-                    Register
-                </Link>}
+                    {button && <Link className='main-button' to="/register">
+                        Register
+                    </Link>}
+                </div>
              </div>
             <Outlet/>
+
         </div>
     )
 }
