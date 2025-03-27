f2fb56eb5e66289ae8dd50cc25712571af17988e
shawn-dsilva <shawn.dsilva_97@outlook.com>
2025-03-27 23:01:12 +0530

    prod ready

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -16,34 +16,32 @@ import {
 // axios.defaults.baseURL = "http://localhost:5000";
 
 //uncomment and set url to your own for prod
-//axios.defaults.baseURL = "https://demos.shawndsilva.com/sessions-auth-app"
+axios.defaults.baseURL = "https://demos.shawndsilva.com/sessions-auth-app";
 
 //Check if user is already logged in
 export const isAuth = () => (dispatch) => {
-
-    axios
-    .get("/api/users/authchecker",{withCredentials:true})
+  axios
+    .get("/api/users/authchecker", { withCredentials: true })
     .then((res) =>
       dispatch({
         type: AUTH_SUCCESS,
-        payload: res.data
+        payload: res.data,
       })
     )
     .catch((err) => {
       dispatch({
-        type: AUTH_FAIL
+        type: AUTH_FAIL,
       });
     });
-
-}
+};
 
 //Register New User
 export const register = ({ name, email, password }) => (dispatch) => {
   // Headers
   const headers = {
     headers: {
-      "Content-Type": "application/json"
-    }
+      "Content-Type": "application/json",
+    },
   };
 
   // Request body
@@ -51,16 +49,18 @@ export const register = ({ name, email, password }) => (dispatch) => {
 
   axios
     .post("/api/users/register", body, headers)
-    .then((res) =>{
-      dispatch(returnStatus(res.data, res.status, 'REGISTER_SUCCESS'));
-      dispatch({ type: IS_LOADING })
+    .then((res) => {
+      dispatch(returnStatus(res.data, res.status, "REGISTER_SUCCESS"));
+      dispatch({ type: IS_LOADING });
     })
     .catch((err) => {
-      dispatch(returnStatus(err.response.data, err.response.status, 'REGISTER_FAIL'))
+      dispatch(
+        returnStatus(err.response.data, err.response.status, "REGISTER_FAIL")
+      );
       dispatch({
-        type: REGISTER_FAIL
+        type: REGISTER_FAIL,
       });
-      dispatch({ type: IS_LOADING })
+      dispatch({ type: IS_LOADING });
     });
 };
 
@@ -69,8 +69,8 @@ export const login = ({ email, password }) => (dispatch) => {
   // Headers
   const headers = {
     headers: {
-      "Content-Type": "application/json"
-    }
+      "Content-Type": "application/json",
+    },
   };
 
   // Request body
@@ -82,24 +82,24 @@ export const login = ({ email, password }) => (dispatch) => {
       console.log(res);
       dispatch({
         type: LOGIN_SUCCESS,
-        payload: res.data
+        payload: res.data,
       });
       dispatch({ type: IS_LOADING });
-    }
-    )
+    })
     .catch((err) => {
-      dispatch(returnStatus(err.response.data, err.response.status, 'LOGIN_FAIL'))
+      dispatch(
+        returnStatus(err.response.data, err.response.status, "LOGIN_FAIL")
+      );
       dispatch({
-        type: LOGIN_FAIL
+        type: LOGIN_FAIL,
       });
-      dispatch({ type: IS_LOADING })
+      dispatch({ type: IS_LOADING });
     });
 };
 
 //Logout User and Destroy session
 export const logout = () => (dispatch) => {
-
-    axios
+  axios
     .delete("/api/users/logout", { withCredentials: true })
     .then((res) =>
       dispatch({
@@ -109,5 +109,4 @@ export const logout = () => (dispatch) => {
     .catch((err) => {
       console.log(err);
     });
-
-}
+};
