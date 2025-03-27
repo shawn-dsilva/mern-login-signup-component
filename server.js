6bd6b9bbc96809676e321d5411c855015a3b372f
shawn-dsilva <shawn.dsilva_97@outlook.com>
2025-03-27 23:14:52 +0530

    backend prod ready

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const router = express.Router();
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
-const helmet = require('helmet');
-const cors = require('cors');
+const helmet = require("helmet");
+const cors = require("cors");
 
 // Constants
 const {
@@ -16,7 +16,7 @@ const {
   SESS_SECRET,
   NODE_ENV,
   IS_PROD,
-  COOKIE_NAME
+  COOKIE_NAME,
 } = require("./config/config");
 const { MongoURI } = require("./config/database");
 const MAX_AGE = 1000 * 60 * 60 * 3; // Three hours
@@ -26,7 +26,7 @@ const MAX_AGE = 1000 * 60 * 60 * 3; // Three hours
 mongoose
   .connect(MongoURI, {
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
   })
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.log(err));
@@ -34,7 +34,7 @@ mongoose
 // setting up connect-mongodb-session store
 const mongoDBstore = new MongoDBStore({
   uri: MongoURI,
-  collection: "mySessions"
+  collection: "mySessions",
 });
 
 // Express Bodyparser
@@ -55,38 +55,37 @@ app.use(
     cookie: {
       maxAge: MAX_AGE,
       sameSite: false,
-      secure: IS_PROD
-    }
+      secure: IS_PROD,
+    },
   })
 );
 
-app.use(helmet())
+app.use(helmet());
 
 // Below corsOptions are for Local development
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: "http://localhost:3000",
   credentials: true,
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 // Below corsOptions work in deployment as Docker containers
 const corsOptionsProd = {
-  origin: 'http://localhost',
+  origin: "http://localhost",
   credentials: true,
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 app.use(cors(corsOptions));
-
 
 router.get("/", (req, res) => res.send("HELLO FRIEND"));
 
 // API / Routes;
 // Uncomment Below for Development
-app.use("/api/users", require("./routes/users"));
+// app.use("/api/users", require("./routes/users"));
 
 //Uncomment Below for Production, routes mounted at /sessions-auth-app and not root domain
-//app.use("/sessions-auth-app/api/users", require("./routes/users"));
+app.use("/sessions-auth-app/api/users", require("./routes/users"));
 // app.use("/api/auth", require("./routes/auth"));
 
 app.listen(PORT, () => console.log(`Server started on http://${HOST}:${PORT}`));
